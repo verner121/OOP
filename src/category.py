afb282cc1cfbf48ddd98e81abce6efbc713fdfc6
Ksenia Kovaleva <kovalevaksenia21@gmail.com>
2025-02-08 14:22:00 +0300

    исправление ошибок по flake8 и исправление теста test_category

--- src/category.py
+++ src/category.py
@@ -10,4 +10,4 @@ class Category:
         self.description = description
         self.products = products if products else []
         Category.category_count += 1
-        Category.product_count = len(products) if products else 0
+        Category.product_count += len(products) if products else 0
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,20 +1,14 @@
 def test_category_init(first_category, last_category):
     assert first_category.name == "Смартфоны"
-    assert (
-            first_category.description
-            == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
-    )
+    assert first_category.description == 'Смартфоны, как средство получения дополнительных функций для удобства жизни'
 
     assert last_category.name == "Телевизоры"
-    assert (
-            last_category.description
-            == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
-    )
+    assert last_category.description == "Современный телевизор станет вашим другом и помощником"
 
     assert len(first_category.products) == 2
 
     assert first_category.category_count == 2
     assert last_category.category_count == 2
 
-    assert first_category.product_count == 2
-    assert last_category.product_count == 1
+    assert first_category.product_count == 3
+    assert last_category.product_count == 3
